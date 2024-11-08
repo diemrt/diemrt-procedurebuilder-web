880043eacb97b7df38cc7232aaf806e88937d35b
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-11-08 17:42:37 +0100

    Aggiungi supporto per React Router e plugin Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import preline from 'preline/plugin'
+import forms from '@tailwindcss/forms'
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/preline/dist/*.js',
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    preline,
+    forms
+  ],
 }
 
