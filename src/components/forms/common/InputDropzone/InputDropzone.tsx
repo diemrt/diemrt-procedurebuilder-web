00c44cfeeb7bcb6a43be24310c6eb660be0596a9
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-11-13 12:29:18 +0100

    Aggiungi gestione degli errori nel componente InputDropzone per file non validi; migliora il feedback visivo durante il caricamento

--- src/components/forms/common/InputDropzone/InputDropzone.tsx
+++ src/components/forms/common/InputDropzone/InputDropzone.tsx
@@ -2,12 +2,15 @@ import { DocumentArrowDownIcon } from "@heroicons/react/24/outline";
 import { DocumentArrowUpIcon } from "@heroicons/react/24/outline";
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import ShowWhen from "../../../ShowWhen/ShowWhen";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
 const InputDropzone = () => {
   const onDrop = useCallback((acceptedFiles: any) => {
     console.log(acceptedFiles);
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, isDragActive, isDragReject } =
+    useDropzone({ onDrop, accept: { "application/json": [] } });
 
   return (
     <div
@@ -15,12 +18,23 @@ const InputDropzone = () => {
       className="px-20 flex flex-col items-center justify-center w-full h-64 p-4 border-2 border-dashed border-blue-300 rounded-lg cursor-pointer bg-blue-50 hover:border-blue-500 focus:outline-none text-blue-400 hover:text-blue-500"
     >
       <input {...getInputProps()} className="hidden" />
-      {isDragActive ? (
-        <p className="flex items-center font-medium text-2xl text-blue-500"><DocumentArrowDownIcon className="animate-bounce me-3 size-6" /> Rilascia il file qui</p>
-      ) : (
-        <p className="flex items-center font-medium text-2xl"><DocumentArrowUpIcon className="me-3 size-6" /> Trascina qui il file</p>
-      )}
-      <small>Sono ammessi solo file con estensione <code>.json</code></small>
+      <ShowWhen condition={isDragActive && !isDragReject}>
+        <p className="flex items-center font-medium text-2xl text-blue-500">
+          <DocumentArrowDownIcon className="animate-bounce me-3 size-6" />{" "}
+          Rilascia il file qui
+        </p>
+      </ShowWhen>
+      <ShowWhen condition={!isDragActive && !isDragReject}>
+        <p className="flex items-center font-medium text-2xl">
+          <DocumentArrowUpIcon className="me-3 size-6" /> Trascina qui il file
+        </p>
+      </ShowWhen>
+      <ShowWhen condition={isDragReject}>
+        <p className="flex items-center font-medium text-2xl text-red-500"><ExclamationTriangleIcon className="me-3 size-6" /> Il file non è valido</p>
+      </ShowWhen>
+      <small>
+        Sono ammessi solo file con estensione <code>.json</code>
+      </small>
     </div>
   );
 };
