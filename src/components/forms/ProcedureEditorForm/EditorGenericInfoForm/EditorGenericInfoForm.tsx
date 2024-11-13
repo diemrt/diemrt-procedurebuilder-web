7a9ae31517a9b82f17ddf63f4b0151ea95cbb963
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-11-14 00:37:43 +0100

    Aggiungi la gestione della validazione nel modulo EditorGenericInfoForm e aggiorna il componente Section per riflettere lo stato di validità

--- src/components/forms/ProcedureEditorForm/EditorGenericInfoForm/EditorGenericInfoForm.tsx
+++ src/components/forms/ProcedureEditorForm/EditorGenericInfoForm/EditorGenericInfoForm.tsx
@@ -1,15 +1,17 @@
-import { useFormContext } from "react-hook-form";
+import { useFormContext, useFormState } from "react-hook-form";
 import Input from "../../common/Input/Input";
 import Section from "../../common/Section/Section";
 import Textarea from "../../common/Textarea/Textarea";
 
 const EditorGenericInfoForm = () => {
   const { register } = useFormContext();
+  const { isValid } = useFormState();
 
   return (
     <Section
       title="Informazioni generali"
       description="Compila tutti i dati obbligatori, necessari al funzionamento minimo"
+      isValid={isValid}
     >
       <div className="col-span-2">
         <div className="md:grid md:grid-cols-3 md:gap-5">
--- src/components/forms/common/Section/Section.tsx
+++ src/components/forms/common/Section/Section.tsx
@@ -1,15 +1,32 @@
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
+import { XCircleIcon } from "@heroicons/react/24/outline";
+import ShowWhen from "../../../ShowWhen/ShowWhen";
 
-interface Props{
-    title: string;
-    description?: string;
-    children: React.ReactNode;
+interface Props {
+  title: string;
+  description?: string;
+  children: React.ReactNode;
+  isValid?: boolean;
 }
-const Section = ({children, title, description}:Props) => {
+
+const Section = ({ children, title, description, isValid = false }: Props) => {
+  const borderColor = isValid ? "green-500" : "gray-500";
+  const bgColor = isValid ? "green-50" : "gray-50";
+  const textColor = isValid ? "text-green-500" : "text-gray-500";
+
   return (
     <div className="max-w-[85rem] pt-10 lg:pt-14 mx-auto">
-      <div className="md:grid md:grid-cols-3 md:gap-12 xl:gap-32 border-t pt-8">
+      <div
+        className={`md:grid md:grid-cols-3 md:gap-12 xl:gap-32 border border-${borderColor} bg-${bgColor} rounded-xl p-8`}
+      >
         <div className="mb-6">
-          <h2 className="font-semibold text-sm text-gray-800 dark:text-neutral-200">
+          <h2 className={`font-bold text-sm ${textColor} flex items-center`}>
+            <ShowWhen condition={!isValid}>
+              <XCircleIcon className="me-2 size-5"/>
+            </ShowWhen>
+            <ShowWhen condition={isValid}>
+              <CheckCircleIcon className="me-2 size-5" />
+            </ShowWhen>
             {title}
           </h2>
 
@@ -20,7 +37,7 @@ const Section = ({children, title, description}:Props) => {
         {children}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Section
+export default Section;
