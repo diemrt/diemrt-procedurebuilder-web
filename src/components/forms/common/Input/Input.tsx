a3e5310fc659f49467dcf5f19bbe51a646647150
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-11-13 18:07:58 +0100

    Aggiorna il componente Input per migliorare il margine inferiore e modifica il colore del bordo

--- src/components/forms/common/Input/Input.tsx
+++ src/components/forms/common/Input/Input.tsx
@@ -21,7 +21,7 @@ const Input = <TFieldValues extends Record<string, unknown>>({
   ...rest
 }: Props<TFieldValues>) => {
   return (
-    <div className="space-y-2 w-full">
+    <div className="space-y-2 w-full mb-3 md:mb-0">
       <label
         htmlFor={name}
         className="inline-block text-sm font-medium text-gray-800 dark:text-neutral-200"
@@ -33,7 +33,7 @@ const Input = <TFieldValues extends Record<string, unknown>>({
       </label>
       <input
         id={name}
-        className="py-2 px-3 pe-11 block w-full border-gray-400 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+        className="py-2 px-3 pe-11 block w-full border-gray-300 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
         {...rest}
         {...(register &&
           register(
