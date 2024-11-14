84b03c44993d11f0e9662319c7c214c82d38a861
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-11-14 02:16:44 +0100

    Migliora il modulo EditorStepsForm con una nuova interfaccia utente e aggiungi avvisi per la rimozione dei passaggi

--- src/components/forms/ProcedureEditorForm/EditorStepsForm/EditorStepsForm.tsx
+++ src/components/forms/ProcedureEditorForm/EditorStepsForm/EditorStepsForm.tsx
@@ -1,84 +1,103 @@
-import { useFormContext, useFieldArray } from 'react-hook-form';
+import { useFormContext, useFieldArray } from "react-hook-form";
 import Section from "../../common/Section/Section";
 import Input from "../../common/Input/Input";
 import Textarea from "../../common/Textarea/Textarea";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 
 const EditorStepsForm = () => {
-    const { control, register } = useFormContext();
-    const { fields, append, remove } = useFieldArray({
-        control,
-        name: 'steps'
-    });
+  const { control, register } = useFormContext();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "steps",
+  });
 
-    const handleAddStep = () => {
-        append({ name: '', description: '', image: '', timeToRead: 0 });
-    };
+  const handleAddStep = () => {
+    append({ name: "", description: "", image: "", timeToRead: 0 });
+  };
 
-    return (
-        <div>
-            {fields.map((field, index) => (
-                <Section
-                    key={field.id}
-                    title={`Passo ${index + 1}`}
-                    description="Fornisci i dettagli per questo passaggio."
-                    icon={<InformationCircleIcon className="me-2 size-5" />}
-                >
-                    <div className="col-span-2">
-                        <div className="md:grid md:grid-cols-3 md:gap-5">
-                            <div className="col-span-full">
-                                <Input
-                                    name={`steps.${index}.name`}
-                                    type="text"
-                                    label="Nome del Passaggio"
-                                    register={register}
-                                    rules={{
-                                        required: "Campo obbligatorio",
-                                    }}
-                                />
-                            </div>
-                            <div className="col-span-full">
-                                <Textarea
-                                    name={`steps.${index}.description`}
-                                    label="Descrizione del Passaggio"
-                                    register={register}
-                                    rules={{
-                                        required: "Campo obbligatorio",
-                                    }}
-                                />
-                            </div>
-                            <div className="col-span-full">
-                                <Input
-                                    name={`steps.${index}.image`}
-                                    type="text"
-                                    label="URL dell'Immagine"
-                                    register={register}
-                                />
-                            </div>
-                            <div className="col-span-full">
-                                <Input
-                                    name={`steps.${index}.timeToRead`}
-                                    type="number"
-                                    label="Tempo di Lettura (minuti)"
-                                    register={register}
-                                />
-                            </div>
-                            <div className="col-span-full">
-                                <button type="button" onClick={() => remove(index)}>Rimuovi Passaggio</button>
-                            </div>
-                        </div>
-                    </div>
-                </Section>
-            ))}
-            <button 
-                type="button" 
-                onClick={handleAddStep} 
-                className="mt-4 px-4 py-2 border-2 border-blue-500 text-blue-500 text-sm font-semibold rounded-full shadow-md hover:bg-blue-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-            >
-                Aggiungi Passaggio
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      {fields.map((field, index) => (
+        <Section
+          key={field.id}
+          title={`Passo ${index + 1}`}
+          description="Ogni passo rappresenta un'azione che l'utente dovrà compiere per completare la procedura."
+          icon={<InformationCircleIcon className="me-2 size-5" />}
+        >
+          <div className="col-span-2">
+            <div className="md:grid md:grid-cols-3 md:gap-5">
+              <div className="col-span-full">
+                <Input
+                  name={`steps.${index}.name`}
+                  type="text"
+                  label="Titolo"
+                  register={register}
+                  rules={{
+                    required: "Campo obbligatorio",
+                  }}
+                />
+              </div>
+              <div className="col-span-full">
+                <Textarea
+                  name={`steps.${index}.description`}
+                  label="Descrizione"
+                  register={register}
+                  rules={{
+                    required: "Campo obbligatorio",
+                  }}
+                />
+              </div>
+              <div className="col-span-full">
+                <Input
+                  name={`steps.${index}.image`}
+                  type="text"
+                  label="URL dell'Immagine"
+                  register={register}
+                  rules={{
+                    required: "Campo obbligatorio",
+                  }}
+                />
+              </div>
+              <div className="col-span-full">
+                <Input
+                  name={`steps.${index}.timeToRead`}
+                  type="number"
+                  label="Tempo di Lettura (minuti)"
+                  register={register}
+                />
+              </div>
+              <div className="col-span-full">
+                <div className="flex flex-col bg-red-50 border border-red-700 shadow-sm rounded-xl p-4 md:p-5 dark:bg-red-200 dark:border-red-700 dark:shadow-neutral-700/70">
+                  <h3 className="font-bold text-sm text-red-800 flex items-center">
+                    Zona pericolosa
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-600 dark:text-neutral-400">
+                    Questa azione è irreversibile, prima di procedere assicurati di voler rimuovere il passo definitivamente.
+                  </p>
+                  <button
+                    type="button"
+                    onClick={() => remove(index)}
+                    className="px-4 py-2 text-xs w-fit mt-4 bg-gray-400 text-white font-semibold rounded-full shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75"
+                  >
+                    Rimuovi passo
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Section>
+      ))}
+      <button
+        type="button"
+        onClick={handleAddStep}
+        className="fixed bottom-10 right-10 px-7 py-3 border rounded-full shadow-md focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 y-1.5 inline-flex items-center gap-x-2 text-sm font-medium bg-blue-500 text-white hover:shadow-xl focus:outline-none disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+      >
+        <PlusCircleIcon className="size-5" />
+        Aggiungi passo
+      </button>
+    </div>
+  );
 };
 
 export default EditorStepsForm;
