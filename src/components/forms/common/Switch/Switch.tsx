329914dbcd6fa054002b1e25a15893ac088e262b
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-11-14 01:02:06 +0100

    Aggiorna lo stile del componente Switch per migliorare la visibilità e l'interazione

--- src/components/forms/common/Switch/Switch.tsx
+++ src/components/forms/common/Switch/Switch.tsx
@@ -25,7 +25,7 @@ const Switch = <TFieldValues extends Record<string, unknown>>({
       <div className="flex items-center">
         <input
           type="checkbox"
-          className="relative w-[35px] h-[21px] bg-gray-100 border-transparent text-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-blue-600 disabled:opacity-50 disabled:pointer-events-none checked:bg-none checked:text-blue-600 checked:border-blue-600 focus:checked:border-blue-600 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-600
+          className="relative w-[35px] h-[21px] bg-gray-300 border-transparent text-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-gray-600 disabled:opacity-50 disabled:pointer-events-none checked:bg-none checked:text-blue-600 checked:border-blue-600 focus:checked:border-blue-600 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-600
   before:inline-block before:size-4 before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:rounded-full before:shadow before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-neutral-400 dark:checked:before:bg-blue-200"
           id={name}
           {...rest}
