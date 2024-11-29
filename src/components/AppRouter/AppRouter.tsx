60869d61fcb3801aa06b504a92cf2eca61743c7b
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-11-29 11:32:04 +0100

    Rimuovi la pagina DemoPage e il relativo routing dall'AppRouter, eliminando anche il file di dati JSON associato

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "../../pages/LandingPage/LandingPage";
 import ReinitializationHelper from "../ReinitializationHelper/ReinitializationHelper.";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
-import DemoPage from "../../pages/DemoPage/DemoPage";
 import ProcedureRenderPage from "../../pages/ProcedureRenderPage/ProcedureRenderPage";
 import ProcedureEditorPage from "../../pages/ProcedureEditorPage/ProcedureEditorPage";
 import ProcedureByLinkPage from "../../pages/ProcedureByLinkPage/ProcedureByLinkPage";
@@ -13,7 +12,6 @@ const AppRouter = () => {
       <ReinitializationHelper>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/procedure/demo" element={<DemoPage />} />
           <Route path="/procedure/editor" element={<ProcedureEditorPage />} />
           <Route path="/procedure/render" element={<ProcedureRenderPage />} />
           <Route path="/procedure/:link" element={<ProcedureByLinkPage />} />
