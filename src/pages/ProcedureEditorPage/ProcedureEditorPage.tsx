0d3fe7c22d71c6366bae9543f28f931e506ea265
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-11-29 15:22:47 +0100

    Aggiungi controllo dello stato di autenticazione nella pagina ProcedureEditor per reindirizzare gli utenti non autenticati alla pagina di login

--- src/pages/ProcedureEditorPage/ProcedureEditorPage.tsx
+++ src/pages/ProcedureEditorPage/ProcedureEditorPage.tsx
@@ -1,12 +1,25 @@
-import DefaultWrapper from "../../components/DefaultWrapper/DefaultWrapper"
-import ProcedureEditorForm from "../../components/forms/ProcedureEditorForm/ProcedureEditorForm"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import DefaultWrapper from "../../components/DefaultWrapper/DefaultWrapper";
+import ProcedureEditorForm from "../../components/forms/ProcedureEditorForm/ProcedureEditorForm";
+import { checkAuthState } from "../../api/firebase/firebaseUtils";
 
 const ProcedureEditorPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    checkAuthState((user) => {
+      if (!user) {
+        navigate("/login");
+      }
+    });
+  }, [navigate]);
+
   return (
     <DefaultWrapper>
-        <ProcedureEditorForm />
+      <ProcedureEditorForm />
     </DefaultWrapper>
-  )
-}
+  );
+};
 
-export default ProcedureEditorPage
+export default ProcedureEditorPage;
